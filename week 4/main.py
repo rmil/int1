fabd2d400ee0b358c34d7b2decc466910bc0d90a
rmil <rhys.milling@googlemail.com>
2020-11-06 15:03:53 +0000

    Naismith support

--- week 4/main.py
+++ week 4/main.py
@@ -6,10 +6,12 @@ class arc():
         self.cost = cost
 
 class castle():
-    def __init__(self, name, neighbours, heuristic):
+    def __init__(self, name, neighbours, sld, altitude):
         self.name = name
         self.neighbours = neighbours
-        self.heuristic = heuristic
+        self.sld = sld # straight line distance, hardcoded distance between node and J currently
+        self.altitude = altitude
+        self.heuristic = sld
 
         # priority queue needs < to be implemented
     def __lt__(self, other):
@@ -17,18 +19,20 @@ class castle():
     
     def __str__(self):
         return self.name
+    def __repr__(self):
+        return self.name
 
 
-A = castle('A', {}, 0)
-B = castle('B', {}, 200)
-C = castle('C', {}, 600)
-D = castle('D', {}, 100)
-E = castle('E', {}, 300)
-F = castle('F', {}, 300)
-G = castle('G', {}, 100)
-H = castle('H', {}, 500)
-I = castle('I', {}, 400)
-J = castle('J', {}, 500)
+A = castle('A', {}, 30, 0)
+B = castle('B', {}, 20, 200)
+C = castle('C', {}, 20, 600)
+D = castle('D', {}, 20, 100)
+E = castle('E', {}, 15, 300)
+F = castle('F', {}, 10, 300)
+G = castle('G', {}, 5, 100)
+H = castle('H', {}, 5, 500)
+I = castle('I', {}, 5, 400)
+J = castle('J', {}, 0, 500)
 
 A.neighbours = {B: 5, C: 5, D: 5}
 B.neighbours = {A: 5, F: 5}
@@ -62,7 +66,35 @@ def ucs(start, goal):
                     fringe.put((new_cost, new_node))
     return (visited)
 
-route = ucs(A, J)[1]
+def a_star(start, goal, heuristic):
+    visited = []
+    fringe = queue.PriorityQueue()
+    fringe.put((start.heuristic, 0, start))
 
-for place in route:
-    print(place)
+    while not fringe.empty():
+        (_, cost, node) = fringe.get()
+        if node not in visited:
+            visited.append(node)
+
+            if node == goal:
+                return((cost, visited))
+            for neighbour in node.neighbours.items():
+                if neighbour[0] not in visited:
+                    new_cost = cost + neighbour[1]
+                    new_node = neighbour[0]
+                    print(str(node) + " -> " + str(neighbour) + " = " + str(heuristic(node, new_node, False) + new_cost))
+                    fringe.put((heuristic(node, new_node, True) + new_cost, new_cost, new_node))
+    return (visited)
+
+def naismith(src, dst, enable_alt):
+    alt_time = 0
+    flat_time = 0
+    if enable_alt:
+        alt_time = (src.altitude - dst.altitude) / 600
+        flat_time = src.neighbours[dst] / 5
+    else:
+        flat_time = src.neighbours[dst]
+    return alt_time + flat_time 
+
+print(ucs(A, J))
+print(a_star(A, J, naismith))
